204b9da986829cd24bf58eb7ccf52989f7ab7736
jawieme <jana.wieme@ugent.be>
2017-09-15 09:53:22 +0200

    basic project

---
+++ Plaswijzer/Plaswijzer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Plaswijzer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls("http://*:50000")
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+ 
+    }
+}
