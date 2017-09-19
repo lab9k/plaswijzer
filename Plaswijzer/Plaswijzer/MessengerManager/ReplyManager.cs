c71a361b35e9965ce75215a467b1e5f8adc7d93d
Jorg Wieme <jorg.wieme@ugent.be>
2017-09-19 10:32:46 +0200

    Update ReplyManager.cs

--- Plaswijzer/Plaswijzer/MessengerManager/ReplyManager.cs
+++ Plaswijzer/Plaswijzer/MessengerManager/ReplyManager.cs
@@ -7,5 +7,23 @@ namespace Plaswijzer.MessengerManager
 {
     public class ReplyManager: IReplyManager
     {
+        private IMessengerApi api;
+        public string lang { get; set; }
+        public DataConstants Constants;
+        private ILocationFactory locationFactory;
+    
+    public void SendWelcomeMessage(long id, string lang)
+        {                 
+            GenericMessage message = new GenericMessage(id, "Welcome have a reply" /*Constants.GetMessage("Welcome", lang)*/);
+            api.SendMessageToUser(message);
+        }
+        
+     public void SendGetLocationButton(long id, string type, string lang)
+        {
+            GenericMessage message = locationFactory.MakeLocationButton(id, type, lang);
+            api.SendMessageToUser(message);
+
+        }
+        
     }
 }
