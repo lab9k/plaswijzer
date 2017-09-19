d33e437c443b42e587d53fba347af95bd198a528
jawieme <jana.wieme@ugent.be>
2017-09-19 11:30:48 +0200

    Message Handler functions

--- Plaswijzer/Plaswijzer/MessengerManager/IReplyManager.cs
+++ Plaswijzer/Plaswijzer/MessengerManager/IReplyManager.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Plaswijzer.MessengerManager
 {
-    interface IReplyManager
+    public interface IReplyManager
     {
     }
 }
--- Plaswijzer/Plaswijzer/MessengerManager/MessageHandler.cs
+++ Plaswijzer/Plaswijzer/MessengerManager/MessageHandler.cs
@@ -3,19 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Plaswijzer.BotData;
+using static Plaswijzer.BotData.MessengerData;
 
 namespace Plaswijzer.MessengerManager
 {
     public class MessageHandler : IMessageHandler
     {
-        public void CheckForKnowText(MessengerData.Messaging message)
+        private IReplyManager replyManager;
+        private ITextHandler textHandler;
+        public string Language_choice { get; set; }
+
+        public MessageHandler(IReplyManager manager, ITextHandler textHandler)
         {
-            throw new NotImplementedException();
+            replyManager = (ReplyManager)manager;
+            this.textHandler = textHandler;
         }
 
-        public MessengerData.Messaging MessageRecognized(MessengerData.Messaging message)
+        /// <summary>
+        /// Looking if it is a normal text message and giving it to the RandomTextHandler. Check-up after checking for payload
+        /// </summary>
+        /// <param name="message"></param>
+        public void CheckForKnowText(Messaging message)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrWhiteSpace(message?.message?.text))
+            {
+                string txt = message.message.text;
+                textHandler.CheckText(message.sender.id, txt);
+            }
         }
+
+
+        /// <summary>
+        /// If messages corresponds to any of the already defined payloads, set the payload
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public Messaging MessageRecognized(Messaging message)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(message?.postback?.payload) && !string.IsNullOrWhiteSpace(message?.message?.text))
+                {
+                    Console.WriteLine("Zoeken naar payload");
+                    string response = textHandler.GetPayload(message.message.text);
+                    if (response != null)
+                    {
+                        SetPayload(message, response);
+                    }
+                }
+                return message;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+
+        public void SetPayload(Messaging msg, String pl)
+        {
+            msg.postback = new Postback { payload = pl };
+        }
+
+
     }
 }
