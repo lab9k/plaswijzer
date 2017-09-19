dc6eaeff2d7d4c726ecc00281a9a7db373d77685
Jorg Wieme <jorg.wieme@ugent.be>
2017-09-19 09:56:34 +0200

    Update ButtonUrl.cs

--- Plaswijzer/Plaswijzer/MessageBuilder/Model/ButtonUrl.cs
+++ Plaswijzer/Plaswijzer/MessageBuilder/Model/ButtonUrl.cs
@@ -7,19 +7,15 @@ namespace Plaswijzer.MessageBuilder.Model
 {
     public class ButtonUrl : IButton
     {
-        public ButtonUrl(string title, string type, string url, bool messenger_extensions, string webview_height_ratio)
+        public ButtonUrl(string title, string type, string url)
         {
             this.type = type; // required
             this.url = url; // required for web_url type
-            this.messenger_extensions = messenger_extensions; // not required
             this.title = title;  // required
-            this.webview_height_ratio = webview_height_ratio; // not required
         }
         public string type { get; set; }
-        public bool messenger_extensions { get; set; }
         public string url { get; set; }
         public string title { get; set; }
-        public string webview_height_ratio { get; set; }
     }
 }
 
