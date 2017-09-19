e64ca628e2209af31d571883646030def73beb19
jawieme <jana.wieme@ugent.be>
2017-09-19 10:01:20 +0200

    merge

--- Plaswijzer/Plaswijzer/MessageBuilder/Model/ButtonUrl.cs
+++ Plaswijzer/Plaswijzer/MessageBuilder/Model/ButtonUrl.cs
@@ -7,15 +7,19 @@ namespace Plaswijzer.MessageBuilder.Model
 {
     public class ButtonUrl : IButton
     {
-        public ButtonUrl(string title, string type, string url)
+        public ButtonUrl(string title, string type, string url, bool messenger_extensions, string webview_height_ratio)
         {
             this.type = type; // required
             this.url = url; // required for web_url type
+            this.messenger_extensions = messenger_extensions; // not required
             this.title = title;  // required
+            this.webview_height_ratio = webview_height_ratio; // not required
         }
         public string type { get; set; }
+        public bool messenger_extensions { get; set; }
         public string url { get; set; }
         public string title { get; set; }
+        public string webview_height_ratio { get; set; }
     }
 }
 
--- Plaswijzer/Plaswijzer/MessengerManager/IReplyManager.cs
+++ Plaswijzer/Plaswijzer/MessengerManager/IReplyManager.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Plaswijzer.MessengerManager
 {
-    public class ReplyManager: IReplyManager
+    interface IReplyManager
     {
     }
 }
