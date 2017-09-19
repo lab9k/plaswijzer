37b82a982e9a8d8e90afe6e6f66256427f8c2f8e
Jorg Wieme <jorg.wieme@ugent.be>
2017-09-19 09:55:58 +0200

    Update DefaultAction.cs

--- Plaswijzer/Plaswijzer/MessageBuilder/Model/DefaultAction.cs
+++ Plaswijzer/Plaswijzer/MessageBuilder/Model/DefaultAction.cs
@@ -7,17 +7,13 @@ namespace Plaswijzer.MessageBuilder.Model
 {
     public class DefaultAction
     {
-        public DefaultAction(string type, string url, bool messenger_extensions, string webview_height_ratio)
+        public DefaultAction(string type, string url)
         {
             this.type = type; // required
             this.url = url; // required
-            this.messenger_extensions = messenger_extensions; // not required
-            this.webview_height_ratio = webview_height_ratio;
         }
 
         public string type { get; set; }
         public string url { get; set; }
-        public bool messenger_extensions { get; set; }
-        public string webview_height_ratio { get; set; }
     }
 }
