d25c58f4590cc99ecced360bf04abf3e1379cb56
tolauwae <31000331+tolauwae@users.noreply.github.com>
2017-09-21 17:02:31 +0200

    Update ParserKML.cs

--- Plaswijzer/Plaswijzer/Data/ParserKML.cs
+++ Plaswijzer/Plaswijzer/Data/ParserKML.cs
@@ -1,46 +1,101 @@
-﻿using Plaswijzer.Model;
+using Plaswijzer.Model;
+using Plaswijzer.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Plaswijzer.Data
 {
     public class ParserKML: IParserKML
     {
-        public DogToilet[] Dogtoilets { get; set; }
-        public GehandToilet[] GehandToilets { get; set; }
-        public Toilet[] Toilets { get; set; }
-        public Urinoir[] Urinoirs { get; set; }
+        //private String[] keywords;
+
+        //private Dictionary<String, IToilet> toiletFactory;
+
+        public List<DogToilet> Dogtoilets { get; set; }
+        public List<GehandToilet> GehandToilets { get; set; }
+        public List<Toilet> Toilets { get; set; }
+        public List<Urinoir> Urinoirs { get; set; }
 
         public ParserKML()
         {
-            //hier de private methodes oproepen om het parsen te maken
-            //de methodes moeten zorgen dat je arrays gevuld worden
-            OpenFile();
-            ReadInformationDogToilets();
-            ReadInformationDogToilets();
+            // Initialise fields
+            Dogtoilets = new List<DogToilet>();
+            GehandToilets = new List<GehandToilet>();
+            Toilets = new List<Toilet>();
+            Urinoirs = new List<Urinoir>();
 
-            Toilets = new Toilet[2]
+            parseHuman();
+        }
+
+        private void parseHuman()
+        {
+            // Get the xml document
+            var kmlNameSpace = "{http://www.opengis.net/kml/2.2}";
+            var publieksanitair = XDocument.Load("https://datatank.stad.gent/4/infrastructuur/publieksanitair.kml");
+
+            // Parse the document
+            foreach (var toilet in publieksanitair.Elements().Elements().Elements().Elements($"{kmlNameSpace}Placemark"))
+            {
+                // Get the fields
+                var props = new Dictionary<String, XElement>();
+
+                foreach (var prop in toilet.Element($"{kmlNameSpace}ExtendedData").Element($"{kmlNameSpace}SchemaData").Elements())
                 {
-            new Toilet{ID="PS_30",Lon=6.710278911258f, Lat=51.0363375047756f, Type="Basic",Situering="Sint-Pietersstation",Open7op7=1, Openuren="4u - 1u",Type_locat="stations NMBS",Gratis=0 },
-            new Toilet { ID = "PS_34", Lon = 3.710278911259f, Lat = 51.1363375047756f, Type = "Basic", Situering = "Sint-Pietersstation2", Open7op7 = 1, Openuren = "24h", Type_locat = "stations NMBS", Gratis = 1 }
-        };
-            
+                    props[prop.FirstAttribute.Value] = prop;
+                }
+
+                var point = toilet.Element($"{kmlNameSpace}Point");
+                var lon = float.Parse(point.Element($"{kmlNameSpace}coordinates").Value.Split(",")[0]);
+                var lat = float.Parse(point.Element($"{kmlNameSpace}coordinates").Value.Split(",")[1]);
+
+                // Create the corresponding object
+                var type = props["type_sanit"].FirstAttribute.Value;
+                if (type.Contains("urinoir")) {
+                    Urinoirs.Add(new Urinoir {
+                        ID = props["IDGENT"].Value,
+                        Lon = lon,
+                        Lat = lat,
+                        Situering = props["SITUERING"].Value == null ? "Gent" : props["SITUERING"].Value,
+                        Open7op7 = props["open7op7da"].Value == "Ja" ? 1 : 0,
+                        Openuren = props["openuren"].Value,
+                        Type_locat = props["type_locat"].Value,
+                        Gratis = props["prijs_toeg"].Value == "gratis" ? 1 : 0
+                    });
+                }
+
+                if (type.Contains("gehand")) {
+                    GehandToilets.Add(new GehandToilet
+                    {
+                        ID = props["IDGENT"].Value,
+                        Lon = lon,
+                        Lat = lat,
+                        Situering = props["SITUERING"].Value == null ? "Gent" : props["SITUERING"].Value,
+                        Open7op7 = props["open7op7da"].Value == "Ja" ? 1 : 0,
+                        Openuren = props["openuren"].Value,
+                        Type_locat = props["type_locat"].Value,
+                        Gratis = props["prijs_toeg"].Value == "gratis" ? 1 : 0
+                    });
+                }
+
+                Toilets.Add(new Toilet
+                {
+                    ID = props["IDGENT"].Value,
+                    Lon = lon,
+                    Lat = lat,
+                    Type = props["type_sanit"].Value,
+                    Situering = props["SITUERING"].Value == null ? "Gent" : props["SITUERING"].Value,
+                    Open7op7 = props["open7op7da"].Value == "Ja" ? 1 : 0,
+                    Openuren = props["openuren"].Value,
+                    Type_locat = props["type_locat"].Value,
+                    Gratis = props["prijs_toeg"].Value == "gratis" ? 1 : 0
+                });
+
+                Console.WriteLine(props["type_sanit"]);
+            }
         }
-
-        private void OpenFile()
-        {
-            //open bestand
-        }
-
-        private void ReadInformationToilets()
-        {
-
-        }
-
-        private void ReadInformationDogToilets() { }
-
 
     }
 }
